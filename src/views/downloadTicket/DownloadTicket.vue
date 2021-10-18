28963598970f222502615c4af744929f61cf21eb
manojditstek <manoj.yadav@ditstek.com>
2021-10-18 10:22:34 +0530

    download ticket

--- src/views/downloadTicket/DownloadTicket.vue
+++ src/views/downloadTicket/DownloadTicket.vue
@@ -3,10 +3,11 @@
     <div v-for="ticket in tcktDetails.data.data.order_item" :key="ticket.id">
         <div v-for="tkt in ticket.ticket" :key="tkt.id">
             <div>Tickets:{{tkt.ticket_config.name}} - {{tkt.ticket_config.face_value}} {{tkt.ticket_config.currency}}</div>
-            <button @click="downloadTkt(tkt.id)">Download Ticket</button>
+            
         </div>
     </div>
     <div>Total Price : {{tcktDetails.data.data.total}}</div>
+    <button @click="downloadTkt()">Download Ticket</button>
 </div>
 </template>
 
@@ -31,14 +32,9 @@ export default {
             return store.state.createdOrder;
         })
 
-        async function downloadTkt(tktId) {
-            console.log("jdhjhjhkhkj", {
-                orderId: orderID.value.id,
-                tktId
-            })
+        async function downloadTkt() {
             await store.dispatch('downloadTicketPdf', {
                 orderId: orderID.value.id,
-                tktId
             })
         }
         return {
