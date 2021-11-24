5ce0c62dce05c4247293258449273063a12f684c
tarun <tarunwadhwawins@gmail.com>
2021-11-24 15:48:26 +0530

    ui changes

--- src/components/singleEvent/timeSlots/ticketList/Tickets.vue
+++ src/components/singleEvent/timeSlots/ticketList/Tickets.vue
@@ -1,146 +1,194 @@
 <template>
-<div class="ticketToggle" :class="toggleButton?'show':''" >
-    <div class="cardWrapper d-flex"  v-if="ticketDscount?ticketDscount.length==0:''" >
-        <div class="detailsCol">
-            <h2>{{ticket?ticket.name:$t('common.ticketNotFound')}} : {{ticket?(ticket.faceValue).toFixed(2):'0.00'}} {{ticket?ticket.currency:'EUR'}}</h2>
-            <p>{{$t('common.inclFees')}} € 0.00 </p>
+  <div class="ticketToggle" :class="toggleButton ? 'show' : ''">
+    <div class="cardWrapper" v-if="ticketDscount ? ticketDscount.length == 0 : ''">
+      <div class="thumbHeading">
+        <h2>
+          {{ ticket ? ticket.name : $t("common.ticketNotFound") }} :
+          {{ ticket ? ticket.faceValue.toFixed(2) : "0.00" }}
+          {{ ticket ? ticket.currency : "EUR" }}
+        </h2>
+      </div>
+      <div class="d-flex">
+        <div class="thumbDesc">
+          <p>{{ $t("common.inclFees") }} € 0.00</p>
         </div>
-        <div class="ticketMessage"  v-if="ticket.availableTickets==''">
-            <div class="ticketMessageInner " :class="ticket.availableTickets!='' ?'':'soldOut'">
-                {{ticket.availableTickets!='' ?'':$t('common.soldOut')}}
-            </div>
+        <div class="ticketMessage" v-if="ticket.availableTickets == ''">
+          <div
+            class="ticketMessageInner"
+            :class="ticket.availableTickets != '' ? '' : 'soldOut'"
+          >
+            {{ ticket.availableTickets != "" ? "" : $t("common.soldOut") }}
+          </div>
         </div>
-        <div  class="limitExceeded" v-if="perUserQuantity!=''">
-            <div class="limitExceededInner">
-            {{perUserQuantity!=''?$t('common.userLimit'):''}}
-            </div>
+        <div class="limitExceeded" v-if="perUserQuantity != ''">
+          <div class="limitExceededInner">
+            {{ perUserQuantity != "" ? $t("common.userLimit") : "" }}
+          </div>
         </div>
         <div class="buttonWrap d-flex">
-            <button class="minusBtn" @click="removeFromCart()">-</button>
-            <span class="dassedIcon">{{itemQuantity?itemQuantity:0}}</span>
-            <button class="plusBtn" :class="perUserQuantity!=''||ticket.availableTickets==''?'disabled':''" :disabled="perUserQuantity!=''||ticket.availableTickets==''" @click="addToCart()">+</button>
+          <button class="minusBtn" @click="removeFromCart()">-</button>
+          <span class="dassedIcon">{{ itemQuantity ? itemQuantity : 0 }}</span>
+          <button
+            class="plusBtn"
+            :class="
+              perUserQuantity != '' || ticket.availableTickets == '' ? 'disabled' : ''
+            "
+            :disabled="perUserQuantity != '' || ticket.availableTickets == ''"
+            @click="addToCart()"
+          >
+            +
+          </button>
         </div>
+      </div>
     </div>
 
     <div v-else>
-    <div class="cardWrapper d-flex" >
-        
-        <div class="detailsCol" @click="toggleButton=!toggleButton">
-            <h2>{{ticket?ticket.name:$t('common.ticketNotFound')}} :{{ticket?(ticket.faceValue).toFixed(2):'0.00'}} {{ticket?ticket.currency:'EUR'}}</h2>
-            <p>{{$t('common.inclFees')}} € 0.00 </p>
+      <div class="cardWrapper">
+        <div class="thumbHeading">
+          <h2>
+            {{ ticket ? ticket.name : $t("common.ticketNotFound") }} :{{
+              ticket ? ticket.faceValue.toFixed(2) : "0.00"
+            }}
+            {{ ticket ? ticket.currency : "EUR" }}
+          </h2>
         </div>
-        <div class="ticketMessage"   v-if="ticket.availableTickets==''">
-            <div class="ticketMessageInner " :class="ticket.availableTickets!='' ?'':'soldOut'">
-                {{ticket.availableTickets!='' ?'':$t('common.soldOut')}}
+        <div class="d-flex">
+          <div class="thumbDesc" @click="toggleButton = !toggleButton">
+            <p>{{ $t("common.inclFees") }} € 0.00</p>
+          </div>
+          <div class="ticketMessage" v-if="ticket.availableTickets == ''">
+            <div
+              class="ticketMessageInner"
+              :class="ticket.availableTickets != '' ? '' : 'soldOut'"
+            >
+              {{ ticket.availableTickets != "" ? "" : $t("common.soldOut") }}
             </div>
-        </div>
-        
-        <div  class="limitExceeded" v-if="perUserQuantity!=''">
+          </div>
+
+          <div class="limitExceeded" v-if="perUserQuantity != ''">
             <div class="limitExceededInner">
-            {{perUserQuantity!=''?$t('common.userLimit'):''}}
+              {{ perUserQuantity != "" ? $t("common.userLimit") : "" }}
             </div>
-        </div>
-        
-        <div class="buttonWrap d-flex">
+          </div>
+
+          <div class="buttonWrap d-flex">
             <button class="minusBtn" @click="removeFromCart()">-</button>
-            <span class="dassedIcon">{{itemQuantity?itemQuantity:0}}</span>
-            <button class="plusBtn" :class="perUserQuantity!=''||ticket.availableTickets==''?'disabled':''" :disabled="perUserQuantity!=''||ticket.availableTickets==''"  @click="addToCart()">+</button>
-        </div>
-        <div class="collapseArrow lightBg" :class="toggleButton?'active':''" @click="toggleButton=!toggleButton" >
+            <span class="dassedIcon">{{ itemQuantity ? itemQuantity : 0 }}</span>
+            <button
+              class="plusBtn"
+              :class="
+                perUserQuantity != '' || ticket.availableTickets == '' ? 'disabled' : ''
+              "
+              :disabled="perUserQuantity != '' || ticket.availableTickets == ''"
+              @click="addToCart()"
+            >
+              +
+            </button>
+          </div>
+          <div
+            class="collapseArrow lightBg"
+            :class="toggleButton ? 'active' : ''"
+            @click="toggleButton = !toggleButton"
+          >
             <i class="fa fa-angle-right"></i>
+          </div>
         </div>
-    </div>
-    <div  v-if="ticketDscount?ticketDscount.length>0:''" v-show="toggleButton" class="toggleList">
-        <div v-if="ticketDscount?ticketDscount.length>0:''">
-            <TicketsWithDiscount :ticket="ticket" :discount="discount" v-for="discount in ticketDscount" :key="discount.id"/>
+      </div>
+      <div
+        v-if="ticketDscount ? ticketDscount.length > 0 : ''"
+        v-show="toggleButton"
+        class="toggleList"
+      >
+        <div v-if="ticketDscount ? ticketDscount.length > 0 : ''">
+          <TicketsWithDiscount
+            :ticket="ticket"
+            :discount="discount"
+            v-for="discount in ticketDscount"
+            :key="discount.id"
+          />
         </div>
+      </div>
     </div>
-    </div>
-</div>
+  </div>
 </template>
 
 <script>
-import {ref, computed} from 'vue'
-import {useStore} from "vuex";
-import TicketsWithDiscount from '../ticketList/TicketsWithDiscount'
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+import TicketsWithDiscount from "../ticketList/TicketsWithDiscount";
 export default {
-    name: 'Tickets',
-    components: {
-        TicketsWithDiscount
-    },
-    props: {
-        ticket: Object,
-        eventName: String,
-        timeSlotId: Number,
-        ticketDscount: Object,
-        eventChartKey: String,
-        venueId:String,
-        startDate:Date,
-        endDate:Date
-    },
+  name: "Tickets",
+  components: {
+    TicketsWithDiscount,
+  },
+  props: {
+    ticket: Object,
+    eventName: String,
+    timeSlotId: Number,
+    ticketDscount: Object,
+    eventChartKey: String,
+    venueId: String,
+    startDate: Date,
+    endDate: Date,
+  },
 
-    setup(props) {
-        const toggleButton = ref(false);
-        const discountTicket = ref();
-        const store = useStore();
-        let cart = computed(function () {
-            return store.state.cart.cartItems
-        });
+  setup(props) {
+    const toggleButton = ref(false);
+    const discountTicket = ref();
+    const store = useStore();
+    let cart = computed(function () {
+      return store.state.cart.cartItems;
+    });
 
-       
+    function addToCart() {
+      let ticket = {
+        item: { ...props.ticket },
+        eventName: props.eventName,
+        timeslot_id: props.timeSlotId,
+        venueId: props.venueId,
+        startDate: props.startDate,
+        endDate: props.endDate,
+      };
+      store.commit("addCartItem", ticket);
+    }
 
-        function addToCart() {
-            let ticket = {
-                item: {...props.ticket},
-                eventName: props.eventName,
-                timeslot_id: props.timeSlotId,
-                venueId:props.venueId,
-                startDate:props.startDate,
-                endDate:props.endDate
+    function removeFromCart() {
+      let ticket = { ...props.ticket, timeSlotId: props.timeSlotId };
+      store.commit("removeCartItem", ticket);
+    }
 
-            };
-            store.commit("addCartItem", ticket);
-        }
+    let itemQuantity = computed(function () {
+      let ticket = props.ticket;
+      let timeSlotId = props.timeSlotId;
+      if (timeSlotId) {
+        let get_ticket = cart.value.filter(
+          (item) => item.id == ticket.id && item.timeSlotId == timeSlotId
+        );
+        return get_ticket[0]?.quantity;
+      } else {
+        let get_ticket = cart.value.filter((item) => item.id == ticket.id);
+        return get_ticket[0]?.quantity;
+      }
+    });
 
-        function removeFromCart() {
-            let ticket = {...props.ticket, timeSlotId: props.timeSlotId}
-            store.commit("removeCartItem", ticket);
-        }
+    let perUserQuantity = computed(function () {
+      let ticket = props.ticket;
+      let perUser = cart.value.filter(
+        (item) => item.quantity == ticket.ticketsPerUser && item.id == ticket.id
+      );
+      return perUser;
+    });
 
-
-        let itemQuantity = computed(function () {
-            let ticket = props.ticket;
-            let timeSlotId = props.timeSlotId
-            if(timeSlotId){
-                let get_ticket = cart.value.filter((item) => item.id == ticket.id && item.timeSlotId == timeSlotId);
-                return get_ticket[0] ?.quantity;
-            }else{
-                let get_ticket = cart.value.filter((item) => item.id == ticket.id);
-                return get_ticket[0] ?.quantity;
-            }
-         
-        })
-
-        let perUserQuantity = computed(function () {
-            let ticket = props.ticket;
-            let perUser = cart.value.filter((item) => item.quantity == ticket.ticketsPerUser && item.id == ticket.id);
-            return perUser;
-        })
-
-        return {
-            toggleButton,
-            itemQuantity,
-            addToCart,
-            removeFromCart,
-            perUserQuantity,
-            discountTicket,
-
-        }
-    },
-
-}
+    return {
+      toggleButton,
+      itemQuantity,
+      addToCart,
+      removeFromCart,
+      perUserQuantity,
+      discountTicket,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/singleEvent/timeSlots/ticketList/TicketsWithDiscount.vue
+++ src/components/singleEvent/timeSlots/ticketList/TicketsWithDiscount.vue
@@ -1,116 +1,125 @@
 <template>
-<div class="cardWrapper d-flex" >
-<div class="detailsCol">
-    <h2>{{discount.name}}:{{discount.value}} {{'EUR'}}</h2>
-    <div class="priceWrap d-flex">
+  <div class="cardWrapper d-flex">
+    <div class="detailsCol pad-0">
+      <h2>{{ discount.name }}:{{ discount.value }} {{ "EUR" }}</h2>
+      <div class="priceWrap d-flex">
         <div class="eventPrice1">
-            <p>{{$t('common.fees')}} € 0.00</p>
-            <p>{{$t('common.total')}} € 0.00</p>
+          <p>{{ $t("common.fees") }} € 0.00</p>
+          <p>{{ $t("common.total") }} € 0.00</p>
         </div>
         <div class="ticketMessage">
-            <div class="ticketMessageInner " :class="discount.availableTickets!='' ?'':'soldOut'">
-                {{discount.availableTickets!='' ?'':$t('common.soldOut')}}
-            </div>
+          <div
+            class="ticketMessageInner"
+            :class="discount.availableTickets != '' ? '' : 'soldOut'"
+          >
+            {{ discount.availableTickets != "" ? "" : $t("common.soldOut") }}
+          </div>
         </div>
-        <div class="buttonWrap d-flex" v-if="ticket.quantity>0">
-            <button class="minusBtn" @click="removeFromCart()">-</button>
-            <span class="dassedIcon">{{itemQuantity?itemQuantity:0}}</span>
-            <button class="plusBtn" :class="itemQuantity>=1||ticket.availableTickets==''?'disabled':''" :disabled="itemQuantity>=1||ticket.availableTickets==''" @click="addToCart()">+</button>
+        <div class="buttonWrap d-flex" v-if="ticket.quantity > 0">
+          <button class="minusBtn" @click="removeFromCart()">-</button>
+          <span class="dassedIcon">{{ itemQuantity ? itemQuantity : 0 }}</span>
+          <button
+            class="plusBtn"
+            :class="itemQuantity >= 1 || ticket.availableTickets == '' ? 'disabled' : ''"
+            :disabled="itemQuantity >= 1 || ticket.availableTickets == ''"
+            @click="addToCart()"
+          >
+            +
+          </button>
         </div>
+      </div>
     </div>
-</div>
-</div>
+  </div>
 </template>
 
 <script>
-import {
-    ref,
-    computed
-} from 'vue'
-import {
-    useStore
-} from "vuex";
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
 export default {
-    name: 'TicketsWithDiscount',
-    components: {},
-    props: {
-        ticket: Object,
-        eventName: String,
-        timeSlotId: String,
-        discount: Object,
-        eventChartKey: String
-    },
+  name: "TicketsWithDiscount",
+  components: {},
+  props: {
+    ticket: Object,
+    eventName: String,
+    timeSlotId: String,
+    discount: Object,
+    eventChartKey: String,
+  },
 
-    setup(props) {
-        const toggleButton = ref(false);
-        const discountTicket = ref()
-        const store = useStore();
-        let cart = computed(function () {
-            return store.state.cart.cartItems
+  setup(props) {
+    const toggleButton = ref(false);
+    const discountTicket = ref();
+    const store = useStore();
+    let cart = computed(function () {
+      return store.state.cart.cartItems;
+    });
+
+    function addToCart() {
+      let data = {
+        discount: props.discount,
+        ticketId: props.ticket.id,
+        timeSlotId: props.timeSlotId,
+      };
+      store.commit("addDiscountToCart", data);
+    }
+
+    function removeFromCart() {
+      let data = {
+        discount: props.discount,
+        ticketId: props.ticket.id,
+        timeSlotId: props.timeSlotId,
+      };
+      store.commit("removeDiscountToCart", data);
+    }
+
+    let discountTkt = computed(function () {
+      let get_ticket = cart.value.map((element) => {
+        get_ticket = element.discounts;
+      });
+      return get_ticket;
+    });
+
+    let itemQuantity = computed(function () {
+      let discountID = props.discount;
+      let ticket = props.ticket;
+      let timeSlotId = props.timeSlotId;
+      let itemQty = null;
+      let get_ticket = null;
+      get_ticket = cart.value.map((element) =>
+        element.discounts.filter(
+          (item) =>
+            item.id === discountID.id &&
+            element.id === ticket.id &&
+            element.timeSlotId === timeSlotId
+        )
+      );
+      get_ticket.forEach((element) => {
+        element.forEach((element) => {
+          itemQty = element.quantity;
         });
+      });
+      return itemQty;
+    });
 
-        function addToCart() {
-            let data={
-                discount:props.discount,
-                ticketId:props.ticket.id,
-                timeSlotId:props.timeSlotId
-            }
-               store.commit("addDiscountToCart", data);
-        }
+    let perUserQuantity = computed(function () {
+      let ticket = props.ticket;
+      let perUser = cart.value.filter(
+        (item) => item.quantity == ticket.ticketsPerUser && item.id == ticket.id
+      );
+      return perUser;
+    });
 
-        function removeFromCart() {
-            let data={
-                discount:props.discount,
-                ticketId:props.ticket.id,
-                timeSlotId:props.timeSlotId
-            }
-            store.commit("removeDiscountToCart",data);
-
-        }
-
-        let discountTkt = computed(function(){
-            let get_ticket= cart.value.map((element) => {
-              get_ticket = element.discounts
-            })
-            return get_ticket;
-        })
-
-        let itemQuantity = computed(function () {
-            let discountID = props.discount;
-            let ticket = props.ticket;
-            let timeSlotId= props.timeSlotId;
-            let itemQty = null;
-            let get_ticket=null;
-            get_ticket= cart.value.map((element) => 
-            element.discounts.filter((item) => item.id===discountID.id && element.id===ticket.id && element.timeSlotId === timeSlotId));
-             get_ticket.forEach(element => {
-                element.forEach(element=>{
-                    itemQty=element.quantity
-                }) 
-            }); 
-            return itemQty 
-        })
-
-        let perUserQuantity = computed(function () {
-             let ticket = props.ticket;
-            let perUser = cart.value.filter((item) => item.quantity == ticket.ticketsPerUser && item.id == ticket.id);
-            return perUser;
-        })
-
-        return {
-            toggleButton,
-            itemQuantity,
-            addToCart,
-            removeFromCart,
-            perUserQuantity,
-            discountTicket,
-            discountTkt
-
-        }
-    },
-
-}
+    return {
+      toggleButton,
+      itemQuantity,
+      addToCart,
+      removeFromCart,
+      perUserQuantity,
+      discountTicket,
+      discountTkt,
+    };
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
